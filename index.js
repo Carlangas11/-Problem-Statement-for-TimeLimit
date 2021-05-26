bd01d82072d0735e3b0fdb0145e07fe5ab8f0beb
Carlangas11 <73184352+Carlangas11@users.noreply.github.com>
2021-05-25 20:41:37 -0400

    fixed double return

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const TimeLimit = (goodTimes, badTimes) => {
     const maxGoodTime = Math.max.apply(null, goodTimes)
     const minBadTime = Math.min.apply(null, badTimes)
-    // console.log(maxGoodTime, minBadTime)
-    if(maxGoodTime<=minBadTime) return minBadTime-1
-    else return -1
+    var minTime = -1
+    if(maxGoodTime<=minBadTime) minTime = minBadTime-1
+    return minTime
    
 }
 
